fae9a5be7617fedf67cdd6509ace66f5dea11334
SunandiniDumpala <sunandini.dumpala@gmail.com>
2017-09-05 06:10:14 -0500

    Add files via upload

--- reaCodingEx/js/controllers/controller.js
+++ reaCodingEx/js/controllers/controller.js
@@ -1,6 +1,6 @@
 angular.module("Rea").controller("dataController",['$scope', '$http',function($scope,$http){
 	$http.get('data.json').then(function(response) {
-        $scope.myData = JSON.stringify(response.data);
+        $scope.myData = response.data;
         console.log($scope.myData);
          }, function myError(response) {
         $scope.myData = response.statusText;

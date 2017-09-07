6ae3ad5a528f2316bacce601281eed8b14eeb306
SunandiniDumpala <sunandini.dumpala@gmail.com>
2017-09-06 20:14:54 -0500

    Add files via upload

--- reaCodingEx/js/controllers/controller.js
+++ reaCodingEx/js/controllers/controller.js
@@ -1,8 +1,39 @@
 angular.module("Rea").controller("dataController",['$scope', '$http',function($scope,$http){
 	$http.get('data.json').then(function(response) {
         $scope.myData = response.data;
-        console.log($scope.myData);
-         }, function myError(response) {
-        $scope.myData = response.statusText;
-    });
+        },	function myError(response) {
+        		$scope.myData = response.statusText;
+        });
+
+		$scope.addProp = (data) => {
+			var len=$scope.myData.saved;
+			var k=0;
+			var x;
+			while(k<len.length){
+				if(len[k].id==data.id){
+					x=true;
+					alert("Property already added. Cannot add a duplicate");
+				}
+				else{
+					x=false;
+				}
+				k++;
+			};
+
+			if(x==false){
+				add(data);
+			}
+			if(len.length==0){
+				add(data);
+			}
+    	};
+
+    	add = (data) => {
+    		$scope.myData.saved.push(data);
+    	}
+
+		$scope.delete = (index) => {
+			$scope.myData.saved.splice(index,1);
+		};    
+
 }]);
